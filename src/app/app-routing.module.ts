dab1f87838ed815dccfab12e868f289eae3eace6
Mukhtarkhan Baibesh <baibesh91@gmail.com>
2023-04-05 19:37:54 +0600

    added login and home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
+    title: 'Login'
+  },
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+    title: 'Home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
