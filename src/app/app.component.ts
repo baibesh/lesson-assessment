84bb8ba71a2415932e9a4bf9a3dfa0099e9750b9
Mukhtarkhan Baibesh <baibesh91@gmail.com>
2023-04-07 10:11:31 +0600

    improve navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-navbar></app-navbar>
     <div class="container">
-      <app-navbar></app-navbar>
       <router-outlet></router-outlet>
     </div>
   `,
